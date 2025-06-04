29beab95a65ac925f26feebdaa3f1fa8973d307e
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-06-04 15:22:33 +0530

    Initial project structure with properties added

---
+++ src/test/java/com/killerexpertise/student/library/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killerexpertise.student.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
